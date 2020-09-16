f3bd51282d4859b15d26ec3d04a2e65c89e1e27d
tzhao <thomas.zhao@amadeus.com>
2020-09-16 12:33:37 +0200

    Add support for redux dev tool

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,4 +2,4 @@ import {createStore, Store} from 'redux';
 import {reducer} from './reducers';
 import {initialState} from './state';
 
-export const store: Store = createStore(reducer, initialState);
+export const store: Store = createStore(reducer, initialState, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
